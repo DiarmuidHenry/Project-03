e49ddaec7f83136a7b020fb6262fc53393cc80ec
DiarmuidHenry <diarmuidhenry@live.co.uk>
2024-02-01 12:36:58 +0000

    feat: Initialise date. Add variables.

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import numpy as np
 
+# Setting up API from Google Sheet
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -12,8 +14,96 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("discovering_ireland")
 
+# Extracting data from counted_distances worksheet
 data = SHEET.worksheet("counted_distances")
 
 counted_distances = data.get_all_values()
 
-print(counted_distances)
+# Convert the values from counted_distances to a NumPy array with integer values
+edge_weights_matrix = np.array(counted_distances, dtype=np.int32)
+
+print(edge_weights_matrix)
+print(edge_weights_matrix[0][1])
+print(type(edge_weights_matrix[0][1]))
+print(type(edge_weights_matrix))
+
+
+# List of town names, in oder
+town_names = [
+    "Ballycastle",
+    "Coleraine",
+    "Derry",
+    "Letterkenny",
+    "Larne",
+    "Ballymena",
+    "Strabane",
+    "Donegal",
+    "Belfast",
+    "Omagh",
+    "Armagh",
+    "Enniskillen",
+    "Sligo",
+    "Belmullet",
+    "Monaghan",
+    "Ballina",
+    "Newry",
+    "Cavan",
+    "Dundalk",
+    "Carrick on Shannon",
+    "Westport",
+    "Knock",
+    "Longford",
+    "Drogheda",
+    "Roscommon",
+    "Trim",
+    "Mullingar",
+    "Clifden",
+    "Athlone",
+    "Ballinasloe",
+    "Dublin",
+    "Tullamore",
+    "Galway",
+    "Naas",
+    "Portlaoise",
+    "Roscrea",
+    "Tullow",
+    "Arklow",
+    "Shannon",
+    "Limerick",
+    "Kilkenny",
+    "Tipperary",
+    "Clonmel",
+    "Wexford",
+    "Tralee",
+    "Waterford",
+    "Rosslare Harbour",
+    "Killarney",
+    "Youghal",
+    "Cork",
+    "Bantry",
+    "Clonakilty"
+]
+
+# Getting number of town cards from size of edge_weight_matrix
+number_of_towns = edge_weights_matrix.shape[0]
+
+# Construct list of all cards, labelled by their corresponding numbers
+all_cards = list(range(1, number_of_towns + 1))
+
+# List of all entry/exit cards. Must be manually enterred
+entry_cards = [
+    5, 9, 31, 39, 47, 50
+]
+# List of all town cards, which is all_cards with entry_cards removed
+town_cards = [i for i in all_cards if i not in entry_cards]
+
+# Declare assigned card variables in global scope
+assigned_town_cards = []
+assigned_entry_cards = []
+dealt_hand = []
+
+
+
+
+
+
